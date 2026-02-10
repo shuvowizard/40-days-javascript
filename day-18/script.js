1e24b8ca0506885587352a52ddca45111c5ce19c
Shuvo <shuvot01794@gmail.com>
2026-02-10 22:54:07 +0600

    Day 18: Learn javascript dom manipulations

--- day-18/script.js
+++ day-18/script.js
@@ -77,3 +77,84 @@ console.log('Day 18: DOM Manipulations');
     imageElem.hasAttribute("src") // true
     imageElem.hasAttribute("height") // false
 }
+
+// Traversing/Navigating DOM
+
+{
+    // parentElement and parentNode - both are same
+    const span = document.getElementById('text');
+
+    console.log('Parent Element', span.parentElement.parentElement);
+    console.log('Parent Node', span.parentNode.parentNode);
+
+    // children and childNodes - both are not same
+    const mainElem = document.getElementById('main-id');
+
+    console.log('Children', mainElem.children); // return HTML Collection
+    console.log('Child Node', mainElem.childNodes); // return Node List
+
+    console.log('First Child', mainElem.firstChild); // return Node
+    console.log('First Child Element', mainElem.firstElementChild); // return Element
+
+    /**
+     * Node: Everything in the DOM tree. Including /n, text, elements, comments etc
+     * Element : Element is only those which mapped to the HTML elements.
+     */
+
+    const headingElm = document.getElementsByID('something-id2');
+
+    // nextSibling & extElementSibling
+    console.log('Next Sibling', headingElm.nextSibling);
+    console.log('Next Element Sibling', headingElm.nextElementSibling);
+
+    // previousSibling & previousElementSibling
+    console.log('Previous Sibling', headingElm.previousSibling);
+    console.log('Previous Element Sibling', headingElm.previousElementSibling);
+}
+
+// Manipulating Styles
+{
+    const pElem = document.getElementById("p-id");
+    console.log(pElem.style)
+    pElem.style.backgroundColor = "pink";
+
+}
+
+// Manipulating Classes
+
+{
+
+    const mainDivElem = document.getElementById("main-id");
+
+    // console.log(mainDivElem.className) // get className
+    // mainDivElem.className = "secondary-class";  // set new className to override the old one
+    // console.log(mainDivElem.className);
+
+    console.log(mainDivElem.classList); // get classList
+
+    mainDivElem.classList.add("test");
+
+    mainDivElem.classList.remove("layout");
+
+    mainDivElem.classList.replace("main-class", "secondary-class");
+
+    console.log("Does it have test?", mainDivElem.classList.contains("test"));
+
+    console.log("Does it have main-class?", mainDivElem.classList.contains("main-class"));
+
+    // toggle() - If class is present, remove it, else add it
+    mainDivElem.classList.toggle("test");
+    mainDivElem.classList.toggle("test");
+}
+
+// Controlling Visibilities
+
+{
+    const mainDivElem = document.getElementById("main-id");
+    //mainDivElem.style.display = "block";
+    //mainDivElem.style.display = "none";
+
+    //mainDivElem.style.visibility = "hidden"
+
+    mainDivElem.style.opacity = "0.5";
+}
