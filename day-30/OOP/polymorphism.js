6d3aa85228d87876d788fc2e5621fd81ed2ed6d6
Shuvo <shuvot01794@gmail.com>
2026-04-01 22:59:37 +0600

    Day 30: Learn OOP with ES6 classes in JavaScript

---
+++ day-30/OOP/polymorphism.js
@@ -0,0 +1,25 @@
+//* Polymorphism
+
+// Definition: Methods behave differently based on object type.
+// In Code: Override a method in child class.
+
+class Car {
+    drive() {
+        console.log("Driving on fuel");
+    }
+}
+
+class ElectricCar extends Car {
+    drive() {
+        console.log("Driving on electricity");
+    }
+}
+
+// const myCars = [new Car(), new ElectricCar()];
+// myCars.forEach(car => car.drive());
+
+const myCar = new Car();
+const myElectricCar = new ElectricCar();
+
+myCar.drive(); // Driving on fuel
+myElectricCar.drive(); // Driving on electricity
